f99877d89d1fc24bcfcc38f947c005d3fe3731af
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-10-13 10:23:49 +0100

    Fix for system.js users

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,98 +9,98 @@ module.exports = {
   externals: {
     'date-fns/end_of_day': {
       root: ['dateFns', 'endOfDay'],
-      commonjs: 'date-fns/end_of_day',
-      commonjs2: 'date-fns/end_of_day'
+      commonjs: 'date-fns/end_of_day/index',
+      commonjs2: 'date-fns/end_of_day/index'
     },
     'date-fns/add_minutes': {
       root: ['dateFns', 'addMinutes'],
-      commonjs: 'date-fns/add_minutes',
-      commonjs2: 'date-fns/add_minutes'
+      commonjs: 'date-fns/add_minutes/index',
+      commonjs2: 'date-fns/add_minutes/index'
     },
     'date-fns/difference_in_days': {
       root: ['dateFns', 'differenceInDays'],
-      commonjs: 'date-fns/difference_in_days',
-      commonjs2: 'date-fns/difference_in_days'
+      commonjs: 'date-fns/difference_in_days/index',
+      commonjs2: 'date-fns/difference_in_days/index'
     },
     'date-fns/start_of_day': {
       root: ['dateFns', 'startOfDay'],
-      commonjs: 'date-fns/start_of_day',
-      commonjs2: 'date-fns/start_of_day'
+      commonjs: 'date-fns/start_of_day/index',
+      commonjs2: 'date-fns/start_of_day/index'
     },
     'date-fns/is_same_day': {
       root: ['dateFns', 'isSameDay'],
-      commonjs: 'date-fns/is_same_day',
-      commonjs2: 'date-fns/is_same_day'
+      commonjs: 'date-fns/is_same_day/index',
+      commonjs2: 'date-fns/is_same_day/index'
     },
     'date-fns/get_day': {
       root: ['dateFns', 'getDay'],
-      commonjs: 'date-fns/get_day',
-      commonjs2: 'date-fns/get_day'
+      commonjs: 'date-fns/get_day/index',
+      commonjs2: 'date-fns/get_day/index'
     },
     'date-fns/start_of_week': {
       root: ['dateFns', 'startOfWeek'],
-      commonjs: 'date-fns/start_of_week',
-      commonjs2: 'date-fns/start_of_week'
+      commonjs: 'date-fns/start_of_week/index',
+      commonjs2: 'date-fns/start_of_week/index'
     },
     'date-fns/add_days': {
       root: ['dateFns', 'addDays'],
-      commonjs: 'date-fns/add_days',
-      commonjs2: 'date-fns/add_days'
+      commonjs: 'date-fns/add_days/index',
+      commonjs2: 'date-fns/add_days/index'
     },
     'date-fns/end_of_week': {
       root: ['dateFns', 'endOfWeek'],
-      commonjs: 'date-fns/end_of_week',
-      commonjs2: 'date-fns/end_of_week'
+      commonjs: 'date-fns/end_of_week/index',
+      commonjs2: 'date-fns/end_of_week/index'
     },
     'date-fns/difference_in_seconds': {
       root: ['dateFns', 'differenceInSeconds'],
-      commonjs: 'date-fns/difference_in_seconds',
-      commonjs2: 'date-fns/difference_in_seconds'
+      commonjs: 'date-fns/difference_in_seconds/index',
+      commonjs2: 'date-fns/difference_in_seconds/index'
     },
     'date-fns/start_of_month': {
       root: ['dateFns', 'startOfMonth'],
-      commonjs: 'date-fns/start_of_month',
-      commonjs2: 'date-fns/start_of_month'
+      commonjs: 'date-fns/start_of_month/index',
+      commonjs2: 'date-fns/start_of_month/index'
     },
     'date-fns/end_of_month': {
       root: ['dateFns', 'endOfMonth'],
-      commonjs: 'date-fns/end_of_month',
-      commonjs2: 'date-fns/end_of_month'
+      commonjs: 'date-fns/end_of_month/index',
+      commonjs2: 'date-fns/end_of_month/index'
     },
     'date-fns/is_same_month': {
       root: ['dateFns', 'isSameMonth'],
-      commonjs: 'date-fns/is_same_month',
-      commonjs2: 'date-fns/is_same_month'
+      commonjs: 'date-fns/is_same_month/index',
+      commonjs2: 'date-fns/is_same_month/index'
     },
     'date-fns/is_same_second': {
       root: ['dateFns', 'isSameSecond'],
-      commonjs: 'date-fns/is_same_second',
-      commonjs2: 'date-fns/is_same_second'
+      commonjs: 'date-fns/is_same_second/index',
+      commonjs2: 'date-fns/is_same_second/index'
     },
     'date-fns/set_hours': {
       root: ['dateFns', 'setHours'],
-      commonjs: 'date-fns/set_hours',
-      commonjs2: 'date-fns/set_hours'
+      commonjs: 'date-fns/set_hours/index',
+      commonjs2: 'date-fns/set_hours/index'
     },
     'date-fns/set_minutes': {
       root: ['dateFns', 'setMinutes'],
-      commonjs: 'date-fns/set_minutes',
-      commonjs2: 'date-fns/set_minutes'
+      commonjs: 'date-fns/set_minutes/index',
+      commonjs2: 'date-fns/set_minutes/index'
     },
     'date-fns/start_of_minute': {
       root: ['dateFns', 'startOfMinute'],
-      commonjs: 'date-fns/start_of_minute',
-      commonjs2: 'date-fns/start_of_minute'
+      commonjs: 'date-fns/start_of_minute/index',
+      commonjs2: 'date-fns/start_of_minute/index'
     },
     'date-fns/difference_in_minutes': {
       root: ['dateFns', 'differenceInMinutes'],
-      commonjs: 'date-fns/difference_in_minutes',
-      commonjs2: 'date-fns/difference_in_minutes'
+      commonjs: 'date-fns/difference_in_minutes/index',
+      commonjs2: 'date-fns/difference_in_minutes/index'
     },
     'date-fns/add_hours': {
       root: ['dateFns', 'addHours'],
-      commonjs: 'date-fns/add_hours',
-      commonjs2: 'date-fns/add_hours'
+      commonjs: 'date-fns/add_hours/index',
+      commonjs2: 'date-fns/add_hours/index'
     }
   },
   module: {
