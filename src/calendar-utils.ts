455afc02e49dcecd5df8d0730cbcc01b68987d9d
etwillbefine <tim.gatzemeier@gmx.de>
2018-03-08 19:31:17 +0100

    feat: make event color optional

--- src/calendar-utils.ts
+++ src/calendar-utils.ts
@@ -66,7 +66,7 @@ export interface CalendarEvent<MetaType = any> {
   start: Date;
   end?: Date;
   title: string;
-  color: EventColor;
+  color?: EventColor;
   actions?: EventAction[];
   allDay?: boolean;
   cssClass?: string;
