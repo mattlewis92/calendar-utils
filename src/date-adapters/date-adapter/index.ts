d4e5cc9f4f5cf7359dc3c321e52d9beba7abef4d
Matt Lewis <hello@mattlewis.me>
2019-08-23 16:25:28 +0100

    fix: remove string option from date adapter arguments

--- src/date-adapters/date-adapter/index.ts
+++ src/date-adapters/date-adapter/index.ts
@@ -1,75 +1,63 @@
 export interface DateAdapter {
-  addDays(date: Date | string | number, amount: number): Date;
+  addDays(date: Date | number, amount: number): Date;
 
-  addHours(date: Date | string | number, amount: number): Date;
+  addHours(date: Date | number, amount: number): Date;
 
-  addMinutes(date: Date | string | number, amount: number): Date;
+  addMinutes(date: Date | number, amount: number): Date;
 
-  addSeconds(date: Date | string | number, amount: number): Date;
+  addSeconds(date: Date | number, amount: number): Date;
 
-  differenceInDays(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
-  ): number;
+  differenceInDays(dateLeft: Date | number, dateRight: Date | number): number;
 
   differenceInMinutes(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): number;
 
   differenceInSeconds(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): number;
 
-  endOfDay(date: Date | string | number): Date;
+  endOfDay(date: Date | number): Date;
 
-  endOfMonth(date: Date | string | number): Date;
+  endOfMonth(date: Date | number): Date;
 
   endOfWeek(
-    date: Date | string | number,
+    date: Date | number,
     options?: {
       weekStartsOn?: number;
     }
   ): Date;
 
-  getDay(date: Date | string | number): number;
+  getDay(date: Date | number): number;
 
-  getMonth(date: Date | string | number): number;
+  getMonth(date: Date | number): number;
 
-  isSameDay(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
-  ): boolean;
+  isSameDay(dateLeft: Date | number, dateRight: Date | number): boolean;
 
-  isSameMonth(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
-  ): boolean;
+  isSameMonth(dateLeft: Date | number, dateRight: Date | number): boolean;
 
-  isSameSecond(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
-  ): boolean;
+  isSameSecond(dateLeft: Date | number, dateRight: Date | number): boolean;
 
   max(dates: Array<Date | number>): Date;
 
-  setHours(date: Date | string | number, hours: number): Date;
+  setHours(date: Date | number, hours: number): Date;
 
-  setMinutes(date: Date | string | number, minutes: number): Date;
+  setMinutes(date: Date | number, minutes: number): Date;
 
-  getHours(date: Date | string | number): number;
+  getHours(date: Date | number): number;
 
-  getMinutes(date: Date | string | number): number;
+  getMinutes(date: Date | number): number;
 
-  startOfDay(date: Date | string | number): Date;
+  startOfDay(date: Date | number): Date;
 
-  startOfMinute(date: Date | string | number): Date;
+  startOfMinute(date: Date | number): Date;
 
-  startOfMonth(date: Date | string | number): Date;
+  startOfMonth(date: Date | number): Date;
 
   startOfWeek(
-    date: Date | string | number,
+    date: Date | number,
     options?: {
       weekStartsOn?: number;
     }
--- src/date-adapters/moment/index.ts
+++ src/date-adapters/moment/index.ts
@@ -1,94 +1,94 @@
 import { DateAdapter } from '../date-adapter';
 
 export function adapterFactory(moment): DateAdapter {
-  function addDays(date: Date | string | number, amount: number) {
+  function addDays(date: Date | number, amount: number) {
     return moment(date)
       .add(amount, 'days')
       .toDate();
   }
 
-  function addHours(date: Date | string | number, amount: number) {
+  function addHours(date: Date | number, amount: number) {
     return moment(date)
       .add(amount, 'hours')
       .toDate();
   }
 
-  function addMinutes(date: Date | string | number, amount: number) {
+  function addMinutes(date: Date | number, amount: number) {
     return moment(date)
       .add(amount, 'minutes')
       .toDate();
   }
 
-  function addSeconds(date: Date | string | number, amount: number): Date {
+  function addSeconds(date: Date | number, amount: number): Date {
     return moment(date)
       .add(amount, 'seconds')
       .toDate();
   }
 
   function differenceInDays(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): number {
     return moment(dateLeft).diff(moment(dateRight), 'days');
   }
 
   function differenceInMinutes(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): number {
     return moment(dateLeft).diff(moment(dateRight), 'minutes');
   }
 
   function differenceInSeconds(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): number {
     return moment(dateLeft).diff(moment(dateRight), 'seconds');
   }
 
-  function endOfDay(date: Date | string | number): Date {
+  function endOfDay(date: Date | number): Date {
     return moment(date)
       .endOf('day')
       .toDate();
   }
 
-  function endOfMonth(date: Date | string | number): Date {
+  function endOfMonth(date: Date | number): Date {
     return moment(date)
       .endOf('month')
       .toDate();
   }
 
-  function endOfWeek(date: Date | string | number): Date {
+  function endOfWeek(date: Date | number): Date {
     return moment(date)
       .endOf('week')
       .toDate();
   }
 
-  function getDay(date: Date | string | number): number {
+  function getDay(date: Date | number): number {
     return moment(date).day();
   }
 
-  function getMonth(date: Date | string | number): number {
+  function getMonth(date: Date | number): number {
     return moment(date).month();
   }
 
   function isSameDay(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): boolean {
     return moment(dateLeft).isSame(moment(dateRight), 'day');
   }
 
   function isSameMonth(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): boolean {
     return moment(dateLeft).isSame(moment(dateRight), 'month');
   }
 
   function isSameSecond(
-    dateLeft: Date | string | number,
-    dateRight: Date | string | number
+    dateLeft: Date | number,
+    dateRight: Date | number
   ): boolean {
     return moment(dateLeft).isSame(moment(dateRight), 'second');
   }
@@ -97,47 +97,47 @@ export function adapterFactory(moment): DateAdapter {
     return moment.max(dates.map(date => moment(date))).toDate();
   }
 
-  function setHours(date: Date | string | number, hours: number): Date {
+  function setHours(date: Date | number, hours: number): Date {
     return moment(date)
       .hours(hours)
       .toDate();
   }
 
-  function setMinutes(date: Date | string | number, minutes: number): Date {
+  function setMinutes(date: Date | number, minutes: number): Date {
     return moment(date)
       .minutes(minutes)
       .toDate();
   }
 
-  function startOfDay(date: Date | string | number): Date {
+  function startOfDay(date: Date | number): Date {
     return moment(date)
       .startOf('day')
       .toDate();
   }
 
-  function startOfMinute(date: Date | string | number): Date {
+  function startOfMinute(date: Date | number): Date {
     return moment(date)
       .startOf('minute')
       .toDate();
   }
 
-  function startOfMonth(date: Date | string | number): Date {
+  function startOfMonth(date: Date | number): Date {
     return moment(date)
       .startOf('month')
       .toDate();
   }
 
-  function startOfWeek(date: Date | string | number): Date {
+  function startOfWeek(date: Date | number): Date {
     return moment(date)
       .startOf('week')
       .toDate();
   }
 
-  function getHours(date: Date | string | number): number {
+  function getHours(date: Date | number): number {
     return moment(date).hours();
   }
 
-  function getMinutes(date: Date | string | number): number {
+  function getMinutes(date: Date | number): number {
     return moment(date).minutes();
   }
 
